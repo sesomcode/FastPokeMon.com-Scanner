e3c0ff75305a57efbcf349c888a220e1c6504435
sesomcode <moshiko226@hotmail.com>
2016-08-08 08:34:41 +0300

    Update FastPokeMapScanner.js

--- FastPokeMapScanner.js
+++ FastPokeMapScanner.js
@@ -2,16 +2,16 @@
 //To use with fastpokemap.com
 //add script hit run and type in console "startRunner();"
 //to stop "stopRunner();" to resume "startRunner();"
-//to reset and start again from a new point "resetRunner();" and then "startRunner();" again
+//to reset and start again from a new point "resetRunner();" and then "starRunner();" again
 //
 var RefreshRateInSeconds = 1; // update every [value] seconds
 var i = 0;
 var showInConsole = true;
-var siteMarkerLat = window.marker.getLatLng().lat;
-var siteMarkerLng = window.marker.getLatLng().lng;
+var siteMarkerLat = window.marker._latlng.lat;
+var siteMarkerLng = window.marker._latlng.lng;
 var scanNorth   = setInterval(north(), RefreshRateInSeconds * 1000);
 var scanNorthEast   = setInterval(northEast(), RefreshRateInSeconds * 1000);
-var scanNorthWest   = setInterval(nortWest(), RefreshRateInSeconds * 1000);
+var scanNorthWest   = setInterval(northWest(), RefreshRateInSeconds * 1000);
 var scanSouth   = setInterval(south(), RefreshRateInSeconds * 1000);
 var scanSouthEast   = setInterval(southEast(), RefreshRateInSeconds * 1000);
 var scanSouthWest   = setInterval(southWest(), RefreshRateInSeconds * 1000);
@@ -22,8 +22,8 @@ var iIncreament   = setInterval(iIncreamentFunction(),RefreshRateInSeconds * 100
 //Functions
 function iIncreamentFunction() {
          i+=0.0001;
-}
 
+}
 function north() {
         let lat = siteMarkerLat + i;
         let lng = siteMarkerLng;
@@ -31,6 +31,7 @@ function north() {
         if(showInConsole === true){
         console.log("north function: " + lat + " | " + lng);
     }
+
 }
 
 function northEast() {
@@ -40,6 +41,7 @@ function northEast() {
         if(showInConsole === true){
         console.log("northEast function: " + lat + " | " + lng);
     }
+
 }
 
 function northWest() {
@@ -59,8 +61,8 @@ function south() {
         if(showInConsole === true){
         console.log("south function: " + lat + " | " + lng);
     }
-}
 
+}
 function southEast() {
         let lat = siteMarkerLat - i;
         let lng = siteMarkerLng + i;
@@ -68,8 +70,8 @@ function southEast() {
         if(showInConsole === true){
         console.log("southEast function: " + lat + " | " + lng);
     }
-}
 
+}
 function southWest() {
         let lat = siteMarkerLat - i;
         let lng = siteMarkerLng - i;
@@ -77,8 +79,8 @@ function southWest() {
         if(showInConsole === true){
         console.log("southWest function: " + lat + " | " + lng);
     }
-}
 
+}
 function east() {
         let lat = siteMarkerLat;
         let lng = siteMarkerLng + i;
@@ -86,8 +88,8 @@ function east() {
         if(showInConsole === true){
         console.log("east function: " + lat + " | " + lng);
     }
-}
 
+}
 function west() {
         let lat = siteMarkerLat;
         let lng = siteMarkerLng - i;
@@ -95,26 +97,35 @@ function west() {
         if(showInConsole === true){
         console.log("west function: " + lat + " | " + lng);
     }
+
 }
 
 function stopRunner() {
         window.clearInterval(scanNorth);
+        window.clearInterval(scanNorthEast);
+        window.clearInterval(scanNorthWest);
         window.clearInterval(scanSouth);
+        window.clearInterval(scanSouthEast);
+        window.clearInterval(scanSouthWest);
         window.clearInterval(scanEast);
         window.clearInterval(scanWest);
 }
 
 function startRunner() {
         scanNorth   = setInterval(north, RefreshRateInSeconds * 1000);
+        scanNorthEast = setInterval(northEast,RefreshRateInSeconds * 1000);
+        scanNorthWest = setInterval(northWest,RefreshRateInSeconds * 1000);
         scanSouth   = setInterval(south, RefreshRateInSeconds * 1000);
+        scanSouthEast = setInterval(southEast,RefreshRateInSeconds * 1000);
+        scanSouthWest = setInterval(southWest,RefreshRateInSeconds * 1000);
         scanEast    = setInterval(east, RefreshRateInSeconds * 1000);
         scanWest    = setInterval(west, RefreshRateInSeconds * 1000);
         iIncreament   = setInterval(iIncreamentFunction,RefreshRateInSeconds * 1000);
 }
 
 function resetRunner() {
-         siteMarkerLat = window.marker.getLatLng().lat;
-         siteMarkerLng = window.marker.getLatLng().lng;
+         siteMarkerLat = window.marker._latlng.lat;
+         siteMarkerLng = window.marker._latlng.lng;
          i = 0;
          console.log("Reset succsed , current marker lat : " + siteMarkerLat + "    current marker lng: " + siteMarkerLng);
 }
